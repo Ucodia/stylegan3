4f3f193aec90af25f78ae0007d46b59b88116196
Lionel Ringenbach <ucodia@live.com>
2025-01-20 16:35:18 -0800

    Add fakes snapshots to tensorboard

--- training/training_loop.py
+++ training/training_loop.py
@@ -351,7 +351,12 @@ def training_loop(
         # Save image snapshot.
         if (rank == 0) and (image_snapshot_ticks is not None) and (done or cur_tick % image_snapshot_ticks == 0):
             images = torch.cat([G_ema(z=z, c=c, noise_mode='const').cpu() for z, c in zip(grid_z, grid_c)]).numpy()
-            save_image_grid(images, os.path.join(run_dir, f'fakes{cur_nimg//1000:06d}.png'), drange=[-1,1], grid_size=grid_size)
+            snapshot_path = os.path.join(run_dir, f'fakes{cur_nimg//1000:06d}.png')
+            save_image_grid(images, snapshot_path, drange=[-1,1], grid_size=grid_size)
+            if stats_tfevents is not None:
+                image = PIL.Image.open(snapshot_path)
+                image_tensor = torch.from_numpy(np.array(image)).permute(2, 0, 1)  # Convert to CHW format
+                stats_tfevents.add_image('Generated fakes', image_tensor, global_step=cur_nimg//1000)
 
         # Save network snapshot.
         snapshot_pkl = None
