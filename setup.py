e8a38471b1ce9097cba03dc7e06a5931dcafa472
Lionel Ringenbach <ucodia@live.com>
2025-07-18 14:14:11 -0700

    Adding MSVC PATH

--- setup.py
+++ setup.py
@@ -1,9 +1,30 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import os
+import glob
+
+def _find_compiler_bindir():
+    patterns = [
+        'C:/Program Files*/Microsoft Visual Studio/*/Professional/VC/Tools/MSVC/*/bin/Hostx64/x64',
+        'C:/Program Files*/Microsoft Visual Studio/*/BuildTools/VC/Tools/MSVC/*/bin/Hostx64/x64',
+        'C:/Program Files*/Microsoft Visual Studio/*/Community/VC/Tools/MSVC/*/bin/Hostx64/x64',
+        'C:/Program Files*/Microsoft Visual Studio */vc/bin',
+    ]
+    for pattern in patterns:
+        matches = sorted(glob.glob(pattern))
+        if len(matches):
+            return matches[-1]
+    return None
+
+# Make sure we can find the necessary compiler binaries on Windows
+if os.name == 'nt' and os.system("where cl.exe >nul 2>nul") != 0:
+    compiler_bindir = _find_compiler_bindir()
+    if compiler_bindir is None:
+        raise RuntimeError('Could not find MSVC/GCC/CLANG installation on this computer.')
+    os.environ['PATH'] += ';' + compiler_bindir
 
 # Default architectures for broad RTX support including future cards like RTX5090
-os.environ.setdefault('TORCH_CUDA_ARCH_LIST', '7.0;7.5;8.0;8.6;8.9;9.0')
+os.environ.setdefault('TORCH_CUDA_ARCH_LIST', '7.0;7.5;8.0;8.6')
 
 ext_modules = [
     CUDAExtension(
